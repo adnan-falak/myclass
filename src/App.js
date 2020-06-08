67b8a2869a8eea77cb1432ac7b192fe6c7020d17
adnan-falak <adnanfalak2332@gmail.com>
2020-06-08 12:28:44 +0500

    myClass

--- src/App.js
+++ src/App.js
@@ -4,23 +4,48 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1></h1>
   );
 }
 
 export default App;
+
+function showTime() {
+  var sT= new Date();
+  var h= sT.getHours();
+  var m= sT.getMinutes();
+  var s= sT.getSeconds();
+  var mer="AM";
+  	if (h>=12) {
+  		mer="PM";
+  	}
+  	if (h==0) {
+  		h=12;
+  	}
+  	if (h>12) {
+  		h=h-12;
+  	}
+  	if (h<10) {
+  		h="0"+h;
+  	}
+  	if (m<10) {
+  		m="0"+m;
+  	}
+  	if (s<10) {
+  		s="0"+s;
+  	}
+  var time=h+" "+":"+" "+m+" "+":"+" "+s;
+  document.getElementById("main").innerText=time+" "+mer;
+  setTimeout(showTime,1000);
+
+}
+showTime();
+
+function alsoDate() {
+	var date= new Date();
+	var showDate=date.toDateString();
+  document.getElementById('date').innerText=showDate;
+  
+  document.getElementById("date").innerHTML="Digigtal Clock By Adnan"
+}
+alsoDate();
